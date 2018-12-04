a5cf9ce80799d36026af2709c723cf373a417fef
David Charte <fdavidcl@protonmail.com>
2018-12-04 12:28:56 +0100

    Update bib_to_txt.rb

--- bib_to_txt.rb
+++ bib_to_txt.rb
@@ -10,27 +10,35 @@ if ARGV.empty?
   exit 1
 end
 
-substitutions = {
+SUBS = {
   "{\\'a}" => "á",
   "{\\'e}" => "é",
   "{\\'i}" => "í",
   "{\\'o}" => "ó",
   "{\\'u}" => "ú",
+  '{\\"a}' => "ä",
+  '{\\"e}' => "ë",
+  '{\\"i}' => "ï",
+  '{\\"o}' => "ö",
+  '{\\"u}' => "ü",
   "{\\l}" => "ł",
   "{\\L}" => "Ł",
   /\{?\\Textregistered\}?/ => "®",
 }
 
-# style processor
-cp = CiteProc::Processor.new style: 'apa', format: 'text'
-# read bibtex
-b = BibTeX.open(ARGV[0]).to_citeproc
-# sort in alphabetical order
-b.sort_by! { |i| i["author"][0]["family"] }
-# import to processor
-cp.import b
-# render each element
-elements = b.map { |i| i["id"] }.map { |i| cp.render(:bibliography, id: i)[0] }.join "\n"
-substitutions.each_pair { |k,v| elements.gsub!(k, v) }
-puts elements
+def to_citation filename
+  # style processor
+  cp = CiteProc::Processor.new style: 'apa', format: 'text'
+  # read bibtex
+  b = BibTeX.open(filename).to_citeproc
+  # sort in alphabetical order
+  b.sort_by! { |i| i["author"][0]["family"] }
+  # import to processor
+  cp.import b
+  # render each element
+  elements = b.map { |i| i["id"] }.map { |i| cp.render(:bibliography, id: i)[0] }.join "\n"
+  SUBS.each_pair { |k,v| elements.gsub!(k, v) }
+  elements
+end
 
+ARGV.each { |f| puts to_citation(f) }
